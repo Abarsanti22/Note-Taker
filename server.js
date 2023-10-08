ee7c87acf0866baa4d2ecc26195d2822c9103796
Amanda Barsanti <barsanti@Amandas-MacBook-Pro.local>
2023-10-07 21:07:58 -0400

    added use

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname + '/Develop/public')));
+
+
+
+
 
 
 app.listen(PORT, () => {
